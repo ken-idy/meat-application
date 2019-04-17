6672de52f40f79e8d9050e6c11952a75e3977852
ken-idy <kenidysalvador@gmail.com>
2019-04-18 00:11:59 +0200

    add new dependecies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -16,7 +16,7 @@ import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 import 'zone.js/dist/zone';
 
-import 'intl'
-import 'intl/locale-data/jsonp/pt-BR.js'
+// import 'intl'
+// import 'intl/locale-data/jsonp/pt-BR.js'
 import  'web-animations-js/web-animations.min.js'
 
